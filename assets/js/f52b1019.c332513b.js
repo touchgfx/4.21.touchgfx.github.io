a6a055259c53b0a545e21d76e53d539bf8d82447
Kristoffer Witt <kristoffer.witt@st.com>
2023-01-19 10:52:03 +0100

    Updated documentation for 4.21

--- assets/js/f52b1019.c332513b.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunktouchgfx_documentation=self.webpackChunktouchgfx_documentation||[]).push([[6750],{7085:function(e){e.exports=JSON.parse('{"name":"docusaurus-theme-search-algolia","id":"default"}')}}]);
